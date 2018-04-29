d0b3d30050eb7a26eb175b561b648efe8e759923
Logan <35646791+HunterWilson56@users.noreply.github.com>
2018-04-29 19:59:21 -0400

    Delete loganeliete!.js

--- commands/loganeliete!.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports.run = async (client, message, args) => {
-message.delete();
-message.author.send("DO NOT SHARE THIS INVITE LINK! [INVITE]-https://discordapp.com/oauth2/authorize?client_id=439938462708269066&permissions=8&scope=bot THEN upvote and add logan to your server/servers \n Go here to upvote & add loganhttps://discordbots.org/bot/408070424484904960");
-
-}
