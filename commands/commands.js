577b59f36b5000d2349167d280390ba3889aa8c0
Logan <35646791+HunterWilson56@users.noreply.github.com>
2018-05-16 10:50:04 -0400

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -3,7 +3,7 @@ module.exports.run = async (client, message, args) => {
 let serverembed = new Discord.RichEmbed()
 .setFooter(`l!AFK **__DOESNT WORK AT THE MOMENT__**`)
 .setColor("RANDOM")
-.addField("Commands", "``l!invite``|``l!lockdown``|``l!serverinfo``|``l!userinfo``|``l!bugreport``|``l!AFK``|\n |``l!shorten (shortens links)`` |``l!spotify``|``l!dadjoke``|``l!trump``|``l!mute``|\n|``l!dmteacup``|``l!playdough``|``l!bunny``\n|``l!page``|``l!ship``|``l!pizza``|\n|``l!yesorno``|", true)
+.addField("Commands", "``l!invite``|``l!lockdown``|``l!serverinfo``|``l!userinfo``|``l!bugreport``|``l!AFK``|\n |``l!shorten (shortens links)`` |``l!spotify``|``l!dadjoke``|``l!trump``|``l!mute``|\n|``l!dmteacup``|``l!playdough``|``l!bunny``\n|``l!page``|``l!ship``|``l!pizza``|\n|``l!yesorno``|``l!stats``|", true)
 message.channel.send(serverembed);
 
 }
