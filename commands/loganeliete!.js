710ab5c1d78b724355eb8a57ecb29535116eec27
Logan <35646791+HunterWilson56@users.noreply.github.com>
2018-05-16 22:03:07 -0400

    Update loganeliete!.js

--- commands/loganeliete!.js
+++ commands/loganeliete!.js
@@ -2,6 +2,6 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
 message.delete();
-message.author.send("DO NOT SHARE THIS INVITE LINK! [INVITE]-https://discordapp.com/oauth2/authorize?client_id=439938462708269066&permissions=8&scope=bot THEN upvote and add logan to your server/servers \n Go here to upvote & add loganhttps://discordbots.org/bot/408070424484904960");
+message.author.send("DO NOT SHARE THIS INVITE LINK! [INVITE]-https://discordapp.com/oauth2/authorize?client_id=442789884097462283&scope=bot&permissions=104324289 THEN upvote and add logan to your server/servers \n Go here to upvote & add loganhttps://discordbots.org/bot/408070424484904960");
 
 }
