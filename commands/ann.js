15dd7b59762a636dc721679338f3009b0c2dfa16
Logan <35646791+HunterWilson56@users.noreply.github.com>
2018-05-17 22:04:04 -0400

    Create ann.js

---
+++ commands/ann.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    message.channel.send(`= Links =
+• Source Code        :: http://bit.ly/elxiecode
+• Youtube   :: http://bit.ly/elxieyt`, {code: "asciidoc"});
+}
