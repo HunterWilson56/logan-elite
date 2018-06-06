b8614fb853657a15cb294e184f546b9d62a54510
Logan <35646791+HunterWilson56@users.noreply.github.com>
2018-06-06 10:06:54 -0400

    Update ann.js

--- commands/ann.js
+++ commands/ann.js
@@ -1,7 +1,20 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send(`= Links =
-• Source Code        :: http://bit.ly/elxiecode
-• Youtube   :: http://bit.ly/elxieyt`, {code: "asciidoc"});
+
+
+const embed = new Discord.MessageEmbed()
+.setTitle('Server Information')
+.setURL('https://discord.io/elxiedev')
+.addField('•Source Code',':: http://bit.ly/elxiecode)
+.addField('• Youtube ','::http://bit.ly/elxieyt')
+.setFooter('Invite Elxie To You Server!')
+          
+  message.guild.channels.get('442454078954668032').messages.fetch('446855711302090763').then(msg => msg.edit(embed));
+   message.channel.send("Message Has Been Updated!")
+
+
+
+
 }
+
