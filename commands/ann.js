30b95008fb50d287880c6fd3fe3de8f0548003a3
Logan <35646791+HunterWilson56@users.noreply.github.com>
2018-06-06 10:07:14 -0400

    Update ann.js

--- commands/ann.js
+++ commands/ann.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
 const embed = new Discord.MessageEmbed()
 .setTitle('Server Information')
 .setURL('https://discord.io/elxiedev')
-.addField('•Source Code',':: http://bit.ly/elxiecode)
+.addField('•Source Code',':: http://bit.ly/elxiecode')
 .addField('• Youtube ','::http://bit.ly/elxieyt')
 .setFooter('Invite Elxie To You Server!')
           
